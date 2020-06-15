e690f765ac8896362a458c28b067ee31fc48ab34
github username <github email address>
2020-06-15 16:25:12 -0600

    Show Pages Added

---
+++ gnarpool-frontend/driversList.js
@@ -0,0 +1,40 @@
+fetch('http://localhost:3000/drivers')
+  .then(response => response.json())
+  .then(showDrivers)
+
+  const driversList = document.getElementById('drivers-list')
+
+  function showDrivers(drivers){
+    console.log(drivers)
+    drivers.forEach(driver => {
+      
+      let li = document.createElement('li')
+  
+      li.innerHTML = `<a href='driverShow.html?id=${driver.id}'>${driver.name}</a>`
+      
+      driversList.appendChild(li)
+    })
+  }
+
+  function showRiders(riders){
+    console.log(riders)
+    riders.forEach(rider => {
+      
+      let li = document.createElement('li')
+  
+      li.innerHTML = `<a href='riderShow.html?id=${rider.id}'>${rider.name}</a>`
+      
+      ridersList.appendChild(li)
+    })
+  }
+
+  // function createOptions(powers){
+  //   powers.forEach(power => {
+  //     let option = document.createElement('option')
+
+  //     option.innerText = power.name 
+  //     option.value = power.id
+
+  //     dropdown.appendChild(option)
+  //   })
+  // }
